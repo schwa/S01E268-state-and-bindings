ed66046b6741267a37f053c59a3303917e325c42
Jonathan Wight <jwight@mac.com>
2024-11-08 19:48:42 -0800

    Reduce MainActor-ness of AnyBuiltinView.

--- Sources/NotSwiftUI/AnyBuiltinView.swift
+++ Sources/NotSwiftUI/AnyBuiltinView.swift
@@ -1,12 +1,13 @@
-@MainActor
+/// A type-erased View wrapper that converts a View to a BuiltinView.
 internal struct AnyBuiltinView: BuiltinView {
-    @MainActor
     private var buildNodeTree: (Node) -> ()
 
+    @MainActor
     init<V: View>(_ view: V) {
         buildNodeTree = view.buildNodeTree(_:)
     }
 
+    @MainActor
     func _buildNodeTree(_ node: Node) {
         buildNodeTree(node)
     }
